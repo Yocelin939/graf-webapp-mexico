9c268c730045578a802bf0caa181e2a49ce07e03
Yocelin939 <yoselinarandasalvador914@gmail.com>
2025-02-21 19:23:50 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// Código de color actualizado en tiempo real (si se necesita interactividad)
+document.getElementById("verde").style.backgroundColor = "#008000";
+document.getElementById("rojo").style.backgroundColor = "#800000";
